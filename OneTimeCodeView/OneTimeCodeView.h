d3468006db801edd46155c614054eb9afaff7477
Andrii Drobiazko <c2q9450@gmail.com>
2020-05-04 21:03:08 +0300

    Initial Commit

---
+++ OneTimeCodeView/OneTimeCodeView.h
@@ -0,0 +1,19 @@
+//
+//  OneTimeCodeView.h
+//  OneTimeCodeView
+//
+//  Created by Андрей Дробязко on 04.05.2020.
+//  Copyright © 2020 Andrii Drobiazko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OneTimeCodeView.
+FOUNDATION_EXPORT double OneTimeCodeViewVersionNumber;
+
+//! Project version string for OneTimeCodeView.
+FOUNDATION_EXPORT const unsigned char OneTimeCodeViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OneTimeCodeView/PublicHeader.h>
+
+
